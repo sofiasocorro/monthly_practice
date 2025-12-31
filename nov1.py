6549e49d7a88b9b34e55a8957f8b9f1de89ab8a5
Sofia Socorro <socor512224@gmail.com>
2025-12-31 04:13:16 +0000

    1vFV

--- nov1.py
+++ nov1.py
@@ -9,6 +9,10 @@ Handle edge cases such as empty input or single-word phrases gracefully.
 '''
 while True:
     user_phrase = input('Please enter a phrase or sentence: ')
+    #.split() makes the sentence into a list of words
+    #word[0] takes first letter in word
+    #''.join combines the letters together
+    #for word in user_phrase.split() loops, checks for words
     user_acronym = ''.join(word[0].upper() for word in user_phrase.split())
     print(user_acronym)
  
